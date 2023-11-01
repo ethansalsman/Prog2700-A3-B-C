b93c8aa00165f3c08265f0f47bf897d7f09a3c49
ethansalsman <122046127+ethansalsman@users.noreply.github.com>
2023-10-31 21:16:07 -0300

    icon and fetching data

--- partC.js
+++ partC.js
@@ -2,6 +2,7 @@
 (() => {
 
     //create map in leaflet and tie it to the div called 'theMap'
+
     let map = L.map('theMap').setView([42, -60], 4);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -12,5 +13,21 @@
         .bindPopup('This is a sample popup. You can put any html structure in this including extra flight data. You can also swap this icon out for a custom icon. Some png files have been provided for you to use if you wish.')
         .openPopup();
 
+    //create a custom icon
+    let myIcon = L.icon({
+        iconUrl: 'images/plane4-45.png',
+        iconSize: [30, 30],
+        iconAnchor: [15, 15],
+        popupAnchor: [0, -15]
+    });
+
+    // fetch flight data from OpenSky Network API and filter by country code "CA" for Canada
+    fetch('https://opensky-network.org/api/states/all')
+        .then(response => response.json())
+        .then(data => {
+            let canadianAircraft = data.states.filter(aircraft => aircraft[2] === "CA");
+            console.log(canadianAircraft);
+        })
+        .catch(error => console.error(error));
 
 })()
