91f15e75121971d81d93a7daa1696803baeab7ba
ethansalsman <122046127+ethansalsman@users.noreply.github.com>
2023-11-01 10:36:42 -0300

    final

--- partC.js
+++ partC.js
@@ -6,67 +6,24 @@
     let map = L.map('theMap').setView([42, -60], 4);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-    L.marker([42, -60]).addTo(map)
-        .bindPopup('This is a sample popup. You can put any html structure in this including extra flight data. You can also swap this icon out for a custom icon. Some png files have been provided for you to use if you wish.')
-        .openPopup();
-
-    
-
-    // fetch flight data from OpenSky Network API and filter by country code "CA" for Canada
-    fetch('https://opensky-network.org/api/states/all')
-        .then(response => response.json())
-        .then(data => {
-            let canadianAircraft = data.states.filter(aircraft => aircraft[2] === "CA");
-            console.log(canadianAircraft);
-        })
-        .catch(error => console.error(error));
-
-        // display the canadian aircraft on the map in the partC.html file
-       
-    // fetch flight data from OpenSky Network API and filter by country code "CA" for Canada
-
-
-    // transform the canadianAircraft data into GeoJSON format
-    let features = canadianAircraft.map(aircraft => {
-        // gather the data from the aircraft array
-        let lat = aircraft[6];
-        let lon = aircraft[5];
-        let altitude = aircraft[7];
-        let callsign = aircraft[1];
-        // return the GeoJSON feature
-        return {
-            type: 'Feature',
-            geometry: {
-                type: 'Point',
-                // convert the coordinates to GeoJSON format: [longitude, latitude]
-                coordinates: [lon, lat]
-            },
-            properties: {
-                // include the altitude and callsign as properties
-                altitude: altitude,
-                callsign: callsign
-            }
-        };
+    // change the marker icon to plane4-45.png
+    const myIcon = L.icon({
+        iconUrl: 'plane4-45.png',
+        iconSize: [38, 38],       
     });
+
     // create a GeoJSON feature collection
-    let geoJson = {
+    const geoJson = {
         type: 'FeatureCollection',
         features: features
     };
     // log the GeoJSON feature collection
     console.log(geoJson);
 
-    // add the GeoJSON feature collection to the map
-    L.geoJSON(geoJson).addTo(map);
-    map.eachLayer(layer => {
-        if (layer instanceof L.Marker) {
-            map.removeLayer(layer);
-        }
-    });
-
+    
     // add the new markers to the map
     function fetchAircraftData() {
         fetch('https://opensky-network.org/api/states/all')
@@ -97,15 +54,12 @@
                     }
                 };
             });
-            // create a GeoJSON feature collection
-            let geoJson = {
-                type: 'FeatureCollection',
-                features: features
-            };
-            // log the GeoJSON feature collection
+            
+           
+            // log the GeoJSON features collection
             console.log(geoJson);
 
-            // add the GeoJSON feature collection to the map
+            // add the GeoJSON features collection to the map
             L.geoJSON(geoJson, {
                 pointToLayer: function(feature, latlng) {
                     return L.marker(latlng, {icon: myIcon});
